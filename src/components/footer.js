796344ccc74d99c9147d291072ffb6f3c5d7d009
Jorge Gallardo <jorge8486@gmail.com>
2020-10-25 20:37:27 -0400

    creative commons icon in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,8 @@ import creativeCommonsIcon from "../images/svg/creativeCommonsIcon.svg"
 
 const Footer = () => (
   <div id="footer">
-    <creativeCommonsIcon/>
+    {/* <creativeCommonsIcon/> */}
+    <span className="icon"><img src={creativeCommonsIcon} alt="Creative Commons icon"/></span>
     <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="license noreferrer">
       {/* <span class="icon"><svg><use xlinkHref="#icon-cc-by-nc-sa" /></svg></span> */}
       <span className="icon"> </span>
