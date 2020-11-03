a93ada346ab6faae456bd91206660a8b4c386e25
danielgaylord <danielgaylord@gmail.com>
2020-11-03 13:32:40 -0500

    Update gatsby-config.js
    Updated to include svg plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,14 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: `${__dirname}/src/images/svg/`
+        }
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
