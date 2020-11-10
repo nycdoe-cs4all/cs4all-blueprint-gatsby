b04f557c01f352cef04aae74a05fd64d649be13c
Jorge Gallardo <jorge8486@gmail.com>
2020-11-10 09:37:55 -0500

    curriculum images

--- src/pages/curriculum.js
+++ src/pages/curriculum.js
@@ -1,6 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import k2Units from "../assets/images/k2-units.jpg"
+import three5Units from "../assets/images/35-units.jpg"
+import six8Units from "../assets/images/68-units.jpg"
+import elementary_2 from "../assets/images/elementary_2.jpg"
+import three5buildMyCity from "../assets/images/pic1.jpg"
+import sepJr from "../assets/images/pic14.jpg"
+import six8unitOld from "../assets/images/high_2a.jpg"
+import creativeComputing from "../assets/images/creative-computing.gif"
+import creativeWeb from "../assets/images/creative-web.gif"
+import criticalComputing from "../assets/images/critical-computing.png"
+import physicalComputing from "../assets/images/intro-to-pcomp.gif"
+import csDiscoveries from "../assets/images/csDiscoveries.jpg"
+import sepHackathon from "../assets/images/SEP-Hackathon.jpg"
+import bootstrapDataScience from "../assets/images/bootstrap-data-science.png"
+import tangibleInterfaces from "../assets/images/tangible-interfaces.gif"
+import exploringCS from "../assets/images/exploringCS.jpeg"
+import teals from "../assets/images/TEALS.gif"
+import introToCompMedia from "../assets/images/intro-to-comp-media.gif"
+import csP from "../assets/images/cs-principles.jpg"
+import csA from "../assets/images/cs-a.gif"
+import csAndTheCity from "../assets/images/cs-and-the-city.gif"
 
 const Curriculum = () => (
   <Layout>
@@ -15,7 +36,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/units-k2-computer-science-and-me">
-              <div className="preview"><img src="/static/images/assets/k2-units.jpg" alt="grades K-2 innovator unit" /></div>
+              <div className="preview"><img src={k2Units} alt="grades K-2 innovator unit" /></div>
               <h3>
                 <span className="activity-grade">Grades K-2</span>
                 <span className="activity-title">K-2 Integrated Unit: Computing Science and Me</span>
@@ -27,7 +48,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/units-35-creating-from-scratch">
-              <div className="preview"><img src="/static/images/assets/35-units.jpg" alt="grades 3-5 innovator unit" /></div>
+              <div className="preview"><img src={three5Units} alt="grades 3-5 innovator unit" /></div>
               <h3>
                 <span className="activity-grade">Grades 3-5</span>
                 <span className="activity-title">3-5 Integrated Unit: Creating From Scratch</span>
@@ -39,7 +60,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/units-68-innovative-programming">
-              <div className="preview"><img src="/static/images/assets/68-units.jpg" alt="grades 6-8 innovator unit" /></div>
+              <div className="preview"><img src={six8Units} alt="grades 6-8 innovator unit" /></div>
               <h3>
                 <span className="activity-grade">Grades 6-8</span>
                 <span className="activity-title">6-8 Integrated Unit: Innovative Programming</span>
@@ -51,7 +72,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/computational-media-explorer">
-              <div className="preview"><img src="/static/images/assets/elementary_2.jpg" alt="computational media explorer unit" /></div>
+              <div className="preview"><img src={elementary_2} alt="computational media explorer unit" /></div>
               <h3>
                 <span className="activity-grade">Grades K-2</span>
                 <span className="activity-title">Computational Media Explorer: Computing Through Time</span>
@@ -63,7 +84,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/computational-media-creator">
-              <div className="preview"><img src="/static/images/assets/pic1.jpg" alt="computational media creator unit" /></div>
+              <div className="preview"><img src={three5buildMyCity} alt="computational media creator unit" /></div>
               <h3>
                 <span className="activity-grade">Grades 3-5</span>
                 <span className="activity-title">Computational Media Creator: Build My City</span>
@@ -75,7 +96,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/software-engineering-program-jr">
-              <div className="preview"><img src="/static/images/assets/pic14.jpg" alt="Software Engineering Program Jr" /></div>
+              <div className="preview"><img src={sepJr} alt="Software Engineering Program Jr" /></div>
               <h3>
                 <span className="activity-grade">Grades K-5</span>
                 <span className="activity-title">Software Engineering Program Junior</span>
@@ -87,7 +108,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/computational-media-innovator">
-              <div className="preview"><img src="/static/images/assets/high_2a.jpg" alt="Computational Media Innovator" /></div>
+              <div className="preview"><img src={six8unitOld} alt="Computational Media Innovator" /></div>
               <h3>
                 <span className="activity-grade">Grades 6-8</span>
                 <span className="activity-title">Computational Media Innovator: Learning to Program, Programming to Learn</span>
@@ -99,7 +120,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/creative-computing/">
-              <div className="preview"><img src="/static/images/assets/creative-computing.gif" alt="Creative Computing" /></div>
+              <div className="preview"><img src={creativeComputing} alt="Creative Computing" /></div>
               <h3>
                 <span className="activity-grade">Grade 6</span>
                 <span className="activity-title">Creative Computing</span>
@@ -111,7 +132,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/critical-computing/">
-              <div className="preview"><img src="http://cs4all.nyc/wp-content/uploads/2020/02/critical-computing.png" alt="Critical Computing" /></div>
+              <div className="preview"><img src={criticalComputing} alt="Critical Computing" /></div>
               <h3>
                 <span className="activity-grade">Grade 6</span>
                 <span className="activity-title">Critical Computing</span>
@@ -123,7 +144,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/creative-web/">
-              <div className="preview"><img src="/static/images/assets/creative-web.gif" alt="Creative Web" /></div>
+              <div className="preview"><img src={creativeWeb} alt="Creative Web" /></div>
               <h3>
                 <span className="activity-grade">Grade 7</span>
                 <span className="activity-title">Creative Web</span>
@@ -135,7 +156,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/intro-to-pcomp/">
-              <div className="preview"><img src="/static/images/assets/intro-to-pcomp.gif" alt="Introduction to Physical Computing" /></div>
+              <div className="preview"><img src={physicalComputing} alt="Introduction to Physical Computing" /></div>
               <h3>
                 <span className="activity-grade">Grade 7</span>
                 <span className="activity-title">Introduction to Physical Computing</span>
@@ -147,7 +168,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/cs-discoveries">
-              <div className="preview"><img src="https://i.ytimg.com/vi/uQim0hBHco0/mqdefault.jpg" alt="Code.org CS Discoveries" /></div>
+              <div className="preview"><img src={csDiscoveries} alt="Code.org CS Discoveries" /></div>
               <h3>
                 <span className="activity-grade">Grades 6-8</span>
                 <span className="activity-title">Code.org CS Discoveries</span>
@@ -159,7 +180,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/software-engineering-program/">
-              <div className="preview"><img src="http://cs4all.nyc/wp-content/uploads/2016/09/SEP-Hackathon.jpg" alt="Software Engineering Program" /></div>
+              <div className="preview"><img src={sepHackathon} alt="Software Engineering Program" /></div>
               <h3>
                 <span className="activity-grade">Grade 6-12</span>
                 <span className="activity-title">Software Engineering Program</span>
@@ -171,7 +192,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/bootstrap-data-science">
-              <div className="preview"><img src="http://cs4all.nyc/wp-content/uploads/2020/02/bootstrap-data-science.png" alt="Bootstrap Data Science" /></div>
+              <div className="preview"><img src={bootstrapDataScience} alt="Bootstrap Data Science" /></div>
               <h3>
                 <span className="activity-grade">Grades 9-10</span>
                 <span className="activity-title">Bootstrap: Data Science</span>
@@ -183,7 +204,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/exploring-cs">
-              <div className="preview"><img src="https://pbs.twimg.com/profile_images/3176850259/72da25556ca103ca5ad6bf21af7be1ea_400x400.jpeg" alt="Exploring Computer Science" /></div>
+              <div className="preview"><img src={exploringCS} alt="Exploring Computer Science" /></div>
               <h3>
                 <span className="activity-grade">Grades 9-10</span>
                 <span className="activity-title">Exploring Computer Science</span>
@@ -195,7 +216,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/teals-intro-to-cs">
-              <div className="preview"><img src="/static/images/assets/TEALS.gif" alt="TEALS Introduction to Computer Science" /></div>
+              <div className="preview"><img src={teals} alt="TEALS Introduction to Computer Science" /></div>
               <h3>
                 <span className="activity-grade">Grade 9-10</span>
                 <span className="activity-title">TEALS Introduction to Computer Science</span>
@@ -207,7 +228,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/intro-to-computational-media">
-              <div className="preview"><img src="/static/images/assets/intro-to-comp-media.gif" alt="Introduction to Computational Media" /></div>
+              <div className="preview"><img src={introToCompMedia} alt="Introduction to Computational Media" /></div>
               <h3>
                 <span className="activity-grade">Grade 9-10</span>
                 <span className="activity-title">Introduction to Computational Media</span>
@@ -219,7 +240,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/tangible-interfaces">
-              <div className="preview"><img src="/static/images/assets/tangible-interfaces.gif" alt="Introduction to Computational Media" /></div>
+              <div className="preview"><img src={tangibleInterfaces} alt="Introduction to Computational Media" /></div>
               <h3>
                 <span className="activity-grade">Grade 10-11</span>
                 <span className="activity-title">Tangible Interfaces (Pilot)</span>
@@ -231,7 +252,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/cs-principles">
-              <div className="preview"><img src="/static/images/assets/cs-principles.jpg" alt="Computer Science Principles" /></div>
+              <div className="preview"><img src={csP} alt="Computer Science Principles" /></div>
               <h3>
                 <span className="activity-grade">Grades 10-12</span>
                 <span className="activity-title">Computer Science Principles (Advanced Placement optional)</span>
@@ -243,7 +264,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/cs-a">
-              <div className="preview"><img src="/static/images/assets/cs-a.gif" alt="CS A" /></div>
+              <div className="preview"><img src={csA} alt="CS A" /></div>
               <h3>
                 <span className="activity-grade">Grades 11-12</span>
                 <span className="activity-title">Advanced Placement Computer Science A</span>
@@ -255,7 +276,7 @@ const Curriculum = () => (
 
           <li>
             <a href="/curriculum/cs-and-the-city">
-              <div className="preview"><img src="/static/images/assets/cs-and-the-city.gif" alt="CS and the City" /></div>
+              <div className="preview"><img src={csAndTheCity} alt="CS and the City" /></div>
               <h3>
                 <span className="activity-grade">Grades 11-12</span>
                 <span className="activity-title">CS and the City</span>
