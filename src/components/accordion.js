c1ea2323ad2f7d53d366f0acb3562d49a02b003d
danielgaylord <danielgaylord@gmail.com>
2020-12-10 12:27:22 -0500

    Update accordion.js
    Minor fixes

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -3,23 +3,32 @@ import scrollTo from 'gatsby-plugin-smoothscroll'
 
 function Accordion(props) {
   const [height, setHeight] = useState("0px");
-
   const content = useRef(null);
 
   useEffect(() => {
     checkHash();
+    console.log(props.state);
     if(props.state === 'none' || props.state === props.name) {
         open();
     }
   });
 
-  function updateState() {
+  function updateState(e) {
+    e.preventDefault();
     if(props.state === props.name) {
+      window.history.replaceState(null, null, "#")
       props.changeState("none");
+      setHeight("0px");
+      setTimeout(() => {typeof window !== 'undefined' && window.scroll({
+        top: 0,
+        behavior: "smooth"
+      })}, 500);
     } else {
+      window.history.replaceState(null, null, "#" + props.name)
       props.changeState(props.name);
+      setHeight(`${content.current.scrollHeight}px`);
+      setTimeout(() => {scrollTo("#" + props.name)}, 500);
     }
-    return false;
   }
 
   function checkHash() {
@@ -43,7 +52,7 @@ function Accordion(props) {
   return (
     <section id={props.name} className={props.state === props.name ? "accordion-target" : "accordion-target compressed"}>
       <header>
-        <a className="box accordion" href={"#" + props.name} onClick={updateState} >
+        <a className="box accordion" href={"#" + props.name} onClick={e => updateState(e)} >
           <div className="box-content" dangerouslySetInnerHTML={{ __html: props.header }} />
         </a>
       </header>
