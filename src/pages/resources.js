3310a105e937431cd3bf0ff189dcc66817a93d42
Daniel Gaylord <danielgaylord@gmail.com>
2021-01-06 13:54:32 -0500

    Revert "Merge branch 'master' of https://github.com/dylsteck/cs4all-blueprint-gatsby"
    This reverts commit e459d20d71bc8e14db0e0805e6e2c512f1c0ea13, reversing
    changes made to 7e5a4bab86b36666491ff6d1affcfa3b9785d46f.

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,15 +1,29 @@
-import React, {useState} from "react"
-import { StaticQuery, graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 import { Link } from "gatsby"
-import ResourceFilterList from "../components/resource-filter-list"
-import ResourceQuery from "../components/resource-query"
+import moment from "moment"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+const parse = require('html-react-parser')
 
-function Resource(props){
-  const [category, changeCategory] = useState("");
+export const query = graphql`
+  {
+    allWordpressPost {
+      edges {
+        node {
+          id
+          slug
+          title
+          content
+          excerpt
+          date
+        }
+      }
+    }
+  }
+`
 
-  return(
+const Resource = ({ data }) => (
   <Layout>
      <SEO title="Educator Resources" />
      <article id="educator-resources">
@@ -17,15 +31,20 @@ function Resource(props){
            <h2><span className="preamble">educator</span> Resources</h2>
            <p>Answers to common questions, developed by teachers and NYC DOE staff.</p>
        </header>
-       <ResourceFilterList changeCategory={(e) => changeCategory(e)} />
        <div className="article-content">
         <ul className="activity-list">
-          <ResourceQuery category={category} />
+          {data.allWordpressPost.edges.map(post =>
+            <li key={post.node.id}>
+              <Link to={post.node.slug}>
+                <h3 dangerouslySetInnerHTML={{ __html: post.node.title }} />
+                <p style={{display:'none'}}>{moment(post.node.date).format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
+                {post.node.excerpt ? parse(post.node.excerpt) : null}
+              </Link>
+            </li> )}
         </ul>
       </div>
     </article>
   </Layout>
-  )
-}
+)
 
 export default Resource
