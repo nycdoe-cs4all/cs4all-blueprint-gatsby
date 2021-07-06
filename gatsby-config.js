0666df00f3a961223471be1528b20d9d304e258e
Daniel Gaylord <danielgaylord@gmail.com>
2021-07-06 09:25:09 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,21 +8,8 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        /*
-         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
-         * Example : 'demo.wp-api.org' or 'www.example-site.com'
-         */
-        baseUrl: `cs4allblueprint.wordpress.com`,
-        // The protocol. This can be http or https.
-        protocol: `https`,
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the assumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: true,
-        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
-        // This feature is untested for sites hosted on WordPress.com
-        useACF: false,
+        // the only required plugin option for WordPress is the GraphQL url.
+        url: `http://198.199.90.39:8000/graphql`,
       },
     },
     `gatsby-plugin-react-helmet`,
