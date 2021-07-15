e6952255057664825d8743c7718b73c3bc146aed
Daniel Gaylord <danielgaylord@gmail.com>
2021-07-15 14:50:41 -0400

    Updating Curriculum Cards

--- src/components/CurriculumCard.js
+++ src/components/CurriculumCard.js
@@ -17,7 +17,8 @@ const CurriculumCard = props => {
           <span className="activity-title">{title}</span>
         </h3>
         <h4>
-          <span className={available ? "activity-offering inactive" : "activity-offering active"}>CS4All PD Available SY 21-22</span>
+          <span className={available ? "activity-offering active" : "activity-offering inactive"}>CS4All PD Available SY 21-22</span>
+          <span className={available ? "activity-offering inactive" : "activity-offering active"}>&nbsp;</span>
         </h4>
         <span className="activity-devices">{devices}</span>
         <span className="activity-concepts">{concepts}</span>
