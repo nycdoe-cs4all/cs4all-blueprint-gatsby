f4d474712de25a3c223476c58c4a80c6c10eb941
Jorge Gallardo <jorge8486@gmail.com>
2022-06-22 09:19:26 -0400

    tweaked text spacing in curriculum cards

--- src/components/CurriculumCard.js
+++ src/components/CurriculumCard.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { Link } from "gatsby"
 
 const CurriculumCard = props => {
   const { link, pic, grade, title, available, devices, concepts } = props
@@ -8,19 +8,32 @@ const CurriculumCard = props => {
     <li>
       <Link to={link}>
         <div className="preview">
-          <img
-            src={pic}
-            alt=""
-          />
+          <img src={pic} alt="" />
         </div>
         <h3>
           <span className="activity-grade">{grade}</span>
           <span className="activity-title">{title}</span>
         </h3>
-        <h4>
-          <span className={available ? "activity-offering active" : "activity-offering inactive"}>CS4All PD Available SY 21-22</span>
-          <span className={available ? "activity-offering inactive" : "activity-offering active"}>&nbsp;</span>
-        </h4>
+        {/* <h4>
+          <span
+            className={
+              available
+                ? "activity-offering active"
+                : "activity-offering inactive"
+            }
+          >
+            CS4All PD Available SY 21-22
+          </span>
+          <span
+            className={
+              available
+                ? "activity-offering inactive"
+                : "activity-offering active"
+            }
+          >
+            &nbsp;
+          </span>
+        </h4> */}
         <span className="activity-devices">{devices}</span>
         <span className="activity-concepts">{concepts}</span>
       </Link>
